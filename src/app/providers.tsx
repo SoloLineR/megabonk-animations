cf879f0f024455a4e9841aa9ff4dc3e7d5e28c0d
SoloLineR <solo.20151@mail.ru>
2026-01-08 15:29:29 +0300

    clean  up

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,10 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider } from "react-router";
-import { router } from "./router";
-import { queryClient } from "./queryClient";
 import { HoverProvider } from "../shared/context/HoverContext";
+import HomePage from "../page/home-page/home-page";
 
 export function AppProviders() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <HoverProvider>
-        <RouterProvider router={router} />
-      </HoverProvider>
-    </QueryClientProvider>
+    <HoverProvider>
+      <HomePage />
+    </HoverProvider>
   );
 }
